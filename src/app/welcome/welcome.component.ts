5776bdc962d1904df02154bc35dd6a6ea9fb35f7
Hannister <hanny.y@gmail.com>
2021-12-16 18:49:26 +0200

    eye movwment

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('eyeLeft') eyeDivLeft!: ElementRef;
+  @ViewChild('eyeRight') eyeDivRight!: ElementRef;
+
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e:any) {
+
+  }
+
+  eyeMovement(event:MouseEvent){
+    this.moveOneEye(this.eyeDivLeft,event);
+    this.moveOneEye(this.eyeDivRight,event);
+  }
+
+  moveOneEye(divEye: any, event:MouseEvent){
+    let eye = divEye.nativeElement;
+    let x = (eye.getBoundingClientRect().left) + (eye.clientWidth/ 2);
+    let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+
+    let rad = Math.atan2(event.pageX - x, event.pageY - y);
+    let rot = (rad * (180 / Math.PI) * -1) + 180;
+
+    this.renderer.setStyle(divEye.nativeElement, 'transform', `rotate(${rot}deg)`);
   }
 
 
